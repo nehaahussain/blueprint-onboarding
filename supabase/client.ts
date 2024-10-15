a310dfe51faa9b43c9be4b4d2cd53cd4dd6fab9b
Neha <neha_h@berkeley.edu>
2024-10-15 16:22:23 -0700

    [chore] addressed some comments

--- supabase/client.ts
+++ supabase/client.ts
@@ -1,8 +1,12 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabase = createClient(
-  process.env.PUBLIC_SUPABASE_URL ?? '',
-  process.env.PUBLIC_SUPABASE_ANON_KEY ?? '',
-);
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error('Supabase URL or anon key is missing.');
+}
+
+const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
 export default supabase;
