d0cec006a12f377dfa276e23d08751bac9f51e05
Neha <neha_h@berkeley.edu>
2024-10-21 16:04:12 -0700

    [feat] created feed

--- app/page.tsx
+++ app/page.tsx
@@ -1,44 +1,76 @@
 'use client';
 
 import React, { useEffect, useState } from 'react';
+import Link from 'next/link'; // Import Link here
 import supabase from '@/supabase/client';
 import styles from './styles.module.css';
 import '../assets/global.css';
-import { PostItem, ProfileItem } from '@/components/project';
+import { CommentItem, ProfileItem } from '@/components/project';
 
 export interface Post {
+  uuid: string;
   description: string;
   like_count: number;
   username: string;
   created_at: string;
   image_url: string;
+  firstComment?: Comment;
 }
 
-export default function Home() {
+export interface Comment {
+  uuid: string;
+  username: string;
+  comment: string;
+  created_at: string;
+}
+
+export default function Feed() {
   const [postData, setPostData] = useState<Post[] | null>(null);
+
   async function fetchPostData() {
     const { data, error } = await supabase.from('posts').select('*');
-
     if (error) {
-      console.error('Error fetching post:', error);
+      console.error('Error fetching posts:', error);
       return null;
     }
 
-    const formattedPosts = data.map((post: Post) => ({
-      description: post.description,
-      like_count: post.like_count,
-      username: post.username,
-      created_at: new Date(post.created_at).toLocaleDateString(), // Formatting date
-      image_url: post.image_url,
-    })) as Post[];
+    const formattedPosts = await Promise.all(
+      data.map(async (post: Post) => {
+        const { data: commentsData } = await supabase
+          .from('comments')
+          .select('*')
+          .eq('post_uuid', post.uuid)
+          .limit(1)
+          .single();
+
+        return {
+          description: post.description,
+          like_count: post.like_count,
+          username: post.username,
+          created_at: new Date(post.created_at).toLocaleDateString(),
+          image_url: post.image_url,
+          uuid: post.uuid,
+          firstComment: commentsData
+            ? {
+                uuid: commentsData.uuid,
+                username: commentsData.username,
+                comment: commentsData.comment,
+                created_at: new Date(
+                  commentsData.created_at,
+                ).toLocaleDateString(),
+              }
+            : null, // Add first comment if exists
+        } as Post;
+      }),
+    );
 
     return formattedPosts;
   }
 
   useEffect(() => {
     async function fetchData() {
-      const fetchedPost = await fetchPostData();
-      setPostData(fetchedPost);
+      const fetchedPosts = await fetchPostData();
+      setPostData(fetchedPosts);
     }
     fetchData();
   }, []);
@@ -47,24 +79,47 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.content}>
         {!postData ? (
-          <p>Loading post...</p>
+          <p>Loading posts...</p>
         ) : (
-          postData.map((post, index) => (
-            <div key={index} className={styles.postSection}>
-              <ProfileItem username={post.username} date={post.created_at} />
-              <div className={styles.text}>
-                <p>{post.description}</p>
+          postData.map(post => (
+            <Link
+              key={post.uuid}
+              className={styles.noUnderline}
+              href={`/post/${post.uuid}`}
+              passHref
+            >
+              <div className={styles.postSection}>
+                <ProfileItem username={post.username} date={post.created_at} />
+                <div className={styles.text}>
+                  <p>{post.description}</p>
+                </div>
+                <div className={styles.image}>
+                  <img
+                    src={post.image_url}
+                    width={500}
+                    height={500}
+                    alt="Post image"
+                  />
+                </div>
+                <div className={styles.activity}>
+                  <p>View Activity</p>
+                </div>
+                <hr className={styles.line} />
+
+                {post.firstComment && (
+                  <CommentItem
+                    key={post.firstComment.uuid}
+                    username={post.firstComment.username}
+                    date={post.firstComment.created_at}
+                    comment={post.firstComment.comment}
+                  />
+                )}
+                <div className={styles.activity}>
+                  <p>View Activity</p>
+                </div>
+                <hr className={styles.line} />
               </div>
-              <div className={styles.image}>
-                <img
-                  src={post.image_url}
-                  width={500}
-                  height={500}
-                  alt="Post image"
-                />
-              </div>
-              <PostItem likes={`${post.like_count} Likes`} />
-            </div>
+            </Link>
           ))
         )}
       </div>
